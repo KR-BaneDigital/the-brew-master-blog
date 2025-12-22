6375cfbb82a35490c3ae4c19bb4c2fed82d76dc4
KR-BaneDigital <161838922+KR-BaneDigital@users.noreply.github.com>
2025-12-22 17:03:56 -0500

    [Auto-Publish] Index: experience-authentic-italian-coffee-bialetti-moka-express-re

--- lib/blog-data/index.ts
+++ lib/blog-data/index.ts
@@ -1,8 +1,10 @@
 import { post as brew_perfection_how_the_cold_brew_maker_elevates_your_coffee } from './brew-perfection-how-the-cold-brew-maker-elevates-your-coffee.data'
 import { post as unlock_ultimate_comfort_the_recliner_pull_handle_that_transf } from './unlock-ultimate-comfort-the-recliner-pull-handle-that-transf.data'
+import { post as experience_authentic_italian_coffee_bialetti_moka_express_re } from './experience-authentic-italian-coffee-bialetti-moka-express-re.data'
 // Blog Data Index - APPEND-ONLY System
 // 🤖 Backend appends to this file when new posts are built
 
 export const allPosts: any[] = []
+  experience_authentic_italian_coffee_bialetti_moka_express_re,
   unlock_ultimate_comfort_the_recliner_pull_handle_that_transf,
   brew_perfection_how_the_cold_brew_maker_elevates_your_coffee,
