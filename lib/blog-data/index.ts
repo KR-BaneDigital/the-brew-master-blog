7d06edec5d86d20cabff279ecff614aa2f25c224
KR-BaneDigital <161838922+KR-BaneDigital@users.noreply.github.com>
2025-12-22 17:03:47 -0500

    [Auto-Publish] Index: unlock-ultimate-comfort-the-recliner-pull-handle-that-transf

--- lib/blog-data/index.ts
+++ lib/blog-data/index.ts
@@ -1,6 +1,8 @@
 import { post as brew_perfection_how_the_cold_brew_maker_elevates_your_coffee } from './brew-perfection-how-the-cold-brew-maker-elevates-your-coffee.data'
+import { post as unlock_ultimate_comfort_the_recliner_pull_handle_that_transf } from './unlock-ultimate-comfort-the-recliner-pull-handle-that-transf.data'
 // Blog Data Index - APPEND-ONLY System
 // 🤖 Backend appends to this file when new posts are built
 
 export const allPosts: any[] = []
+  unlock_ultimate_comfort_the_recliner_pull_handle_that_transf,
   brew_perfection_how_the_cold_brew_maker_elevates_your_coffee,
