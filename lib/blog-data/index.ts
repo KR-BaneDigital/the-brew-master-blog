3170e1a0a2235555380dbd9f9b85c3ab6b4a21d5
KR-BaneDigital <161838922+KR-BaneDigital@users.noreply.github.com>
2025-12-22 19:07:06 -0500

    [Auto-Publish] Index: awaken-your-inner-barista-the-espresso-maker-that-elevates-m

--- lib/blog-data/index.ts
+++ lib/blog-data/index.ts
@@ -11,10 +11,12 @@ import { post as chantomoo_slippers_step_into_comfort_and_cozy_chic } from './ch
 import { post as whisk_your_worries_away_the_ultimate_egg_whisk_set_review } from './whisk-your-worries-away-the-ultimate-egg-whisk-set-review.data'
 import { post as rowing_blazers_watch_elevate_your_style_with_timeless_precis } from './rowing-blazers-watch-elevate-your-style-with-timeless-precis.data'
 import { post as unlock_effortless_handling_why_lever_star_handle_is_a_game_c } from './unlock-effortless-handling-why-lever-star-handle-is-a-game-c.data'
+import { post as awaken_your_inner_barista_the_espresso_maker_that_elevates_m } from './awaken-your-inner-barista-the-espresso-maker-that-elevates-m.data'
 // Blog Data Index - APPEND-ONLY System
 // 🤖 Backend appends to this file when new posts are built
 
 export const allPosts: any[] = []
+  awaken_your_inner_barista_the_espresso_maker_that_elevates_m,
   unlock_effortless_handling_why_lever_star_handle_is_a_game_c,
   rowing_blazers_watch_elevate_your_style_with_timeless_precis,
   whisk_your_worries_away_the_ultimate_egg_whisk_set_review,
